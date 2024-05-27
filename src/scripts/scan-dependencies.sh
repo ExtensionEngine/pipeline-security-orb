dabf8b3d84dfd790ab652a0695f74dc20f735457
droguljic <1875821+droguljic@users.noreply.github.com>
2024-05-27 18:18:15 +0200

    feat: explicitly ensure package manager in `scan_dependencies`
    Install package manager explicitly using the `core/ensure_pkg_manager`
    command instead through the `core/install_dependencies` command
    since actual dependencies are not needed for the audit command, plus
    this should also benefit the performance.

--- src/scripts/scan-dependencies.sh
+++ src/scripts/scan-dependencies.sh
@@ -3,10 +3,10 @@
 if [[ -n "$SCAN_CMD" ]]; then
   echo "Running custom scan command: $SCAN_CMD"
   eval "$SCAN_CMD"
-elif [[ "$PKG_MANAGER" == "npm" ]]; then
+elif [[ "$CURRENT_PKG_MANAGER" == "npm" ]]; then
   echo "Running npm audit with high audit level omitting dev dependencies"
   npm audit --audit-level=high --omit=dev
-elif [[ "$PKG_MANAGER" == "pnpm" ]]; then
+elif [[ "$CURRENT_PKG_MANAGER" == "pnpm" ]]; then
   echo "Running pnpm audit with high audit level on prod dependencies"
   pnpm audit --audit-level=high --prod
 fi
