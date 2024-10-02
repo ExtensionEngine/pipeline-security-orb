a666b0aa39d93ebb82baae858d87951dce75841a
droguljic <1875821+droguljic@users.noreply.github.com>
2024-10-02 13:39:16 +0200

    feat: base secret detection on the `detect_secrets` command
    Introduce the `detect_secrets` command as the main entry point for
    secret scanning and refactor jobs to utilize the command.
    Replace the deprecated gitleaks `detect` with the `git` and `dir` commands.

--- src/scripts/detect-secrets-dir.sh
+++ src/scripts/detect-secrets-dir.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-echo "Starting the directory scan at path '$DIR_PATH'"
+echo "Starting the directory scan at path '$PARAM_STR_SOURCE'"
 echo "Using exported Gitleaks args '$GITLEAKS_ARGS'"
-eval gitleaks "$GITLEAKS_ARGS" --no-git
+
+eval gitleaks dir "$GITLEAKS_ARGS" "$PARAM_STR_SOURCE"
