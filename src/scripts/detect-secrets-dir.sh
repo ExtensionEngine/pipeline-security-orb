6b37aa968d5e780d3ecbb288d57f0fa23eef3d78
droguljic <1875821+droguljic@users.noreply.github.com>
2024-10-03 17:41:44 +0200

    refactor: change verbosity of the detect secrets scripts
    Update output to be more concise and meaningful.
    Enable debug for the dir command.

--- src/scripts/detect-secrets-dir.sh
+++ src/scripts/detect-secrets-dir.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 echo "Starting the directory scan at path '$PARAM_STR_SOURCE'"
-echo "Using exported Gitleaks args '$GITLEAKS_ARGS'"
 
+set -x
 eval gitleaks dir "$GITLEAKS_ARGS" "$PARAM_STR_SOURCE"
+set +x
--- src/scripts/detect-secrets-git.sh
+++ src/scripts/detect-secrets-git.sh
@@ -2,8 +2,6 @@
 
 EVAL_GITLEAKS_ARGS=$(eval echo "${GITLEAKS_ARGS}")
 
-echo "Starting the repository scan at path '$PARAM_STR_SOURCE'"
-echo "Using exported Gitleaks args '$GITLEAKS_ARGS'"
 echo "Using '$GIT_BASE_BRANCH' as the base branch"
 echo "Using '$GIT_CURRENT_BRANCH' as the current branch"
 
@@ -11,13 +9,11 @@ if [[ "$GIT_BASE_BRANCH" = "$GIT_CURRENT_BRANCH" ]]; then
   # Usually when changes are merged back into a long-lived branch, e.g. trunk
   LOG_OPTS="$PARAM_STR_BASE_REVISION^..$CIRCLE_SHA1"
 
-  echo "The base branch is the current branch"
-
   if [[ -z "$PARAM_STR_BASE_REVISION" ]] || ! git cat-file -e "$PARAM_STR_BASE_REVISION"; then
     LOG_OPTS="HEAD~1^..$CIRCLE_SHA1"
 
     echo "The base revision is empty or invalid"
-    echo "Using HEAD~1 as the base revision"
+    echo "Scanning using 'HEAD~1' as the base revision"
 
   elif [[ "$PARAM_STR_BASE_REVISION" == "$CIRCLE_SHA1" ]]; then
     LOG_OPTS=-1
@@ -25,13 +21,15 @@ if [[ "$GIT_BASE_BRANCH" = "$GIT_CURRENT_BRANCH" ]]; then
     echo "The base revision is the current revision"
     echo "Scanning only last commit"
 
+  else
+    echo "Scanning using the provided base revision '$PARAM_STR_BASE_REVISION'"
+
   fi
 
   EVAL_GITLEAKS_ARGS="$GITLEAKS_ARGS --log-opts=$LOG_OPTS"
 
 else
   # Usually a short lived branch, that is a pull request
-  echo "The base branch is not the current branch"
   echo "Scanning all the commits in the current branch '$GIT_CURRENT_BRANCH'"
 
   EVAL_GITLEAKS_ARGS="$GITLEAKS_ARGS --log-opts=$GIT_BASE_BRANCH..$GIT_CURRENT_BRANCH"
