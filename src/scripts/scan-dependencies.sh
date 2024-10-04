0a22492e68d6e8a44b3892c4ab5f8e0a3aa143f5
droguljic <1875821+droguljic@users.noreply.github.com>
2024-10-04 13:55:42 +0200

    feat: replace the `scan_dependencies` job with the command (#29)
    Replacing the job with the same-named command allows for better
    usability of dependency scanning.

--- src/scripts/scan-dependencies.sh
+++ src/scripts/scan-dependencies.sh
@@ -2,11 +2,17 @@
 
 if [[ -n "$SCAN_CMD" ]]; then
   echo "Running custom scan command: $SCAN_CMD"
+
   eval "$SCAN_CMD"
+
 elif [[ "$CURRENT_PKG_MANAGER" == "npm" ]]; then
   echo "Running npm audit with high audit level omitting dev dependencies"
+
   npm audit --audit-level=high --omit=dev
+
 elif [[ "$CURRENT_PKG_MANAGER" == "pnpm" ]]; then
   echo "Running pnpm audit with high audit level on prod dependencies"
+
   pnpm audit --audit-level=high --prod
+
 fi
