38934ae51316d27f1791ad39d314d5b2f01a065b
droguljic <1875821+droguljic@users.noreply.github.com>
2024-10-04 16:13:12 +0200

    refactor: standardize env variable handling (#30)
    Standardizes environment variable handling by,
    - moving all definitions to the top of a file
    - using curly braces
    - prefixing all variables exported from commands
    - evaluating parameters that default to an env variable

--- src/scripts/export-git-branches.sh
+++ src/scripts/export-git-branches.sh
@@ -3,13 +3,13 @@
 BASE_BRANCH=$(git rev-parse --abbrev-ref origin/HEAD | cut -c8-)
 CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
-if [[ -n $PARAM_STR_BASE_BRANCH ]]; then
-  BASE_BRANCH=$PARAM_STR_BASE_BRANCH
+if [[ -n ${PARAM_STR_BASE_BRANCH} ]]; then
+  BASE_BRANCH=${PARAM_STR_BASE_BRANCH}
 fi
 
-if [[ -n $CIRCLE_BRANCH ]]; then
-  CURRENT_BRANCH=$CIRCLE_BRANCH
+if [[ -n ${CIRCLE_BRANCH} ]]; then
+  CURRENT_BRANCH=${CIRCLE_BRANCH}
 fi
 
-echo "export GIT_BASE_BRANCH='$BASE_BRANCH'" >> "$BASH_ENV"
-echo "export GIT_CURRENT_BRANCH='$CURRENT_BRANCH'" >> "$BASH_ENV"
+echo "export GIT_BASE_BRANCH='${BASE_BRANCH}'" >> "${BASH_ENV}"
+echo "export GIT_CURRENT_BRANCH='${CURRENT_BRANCH}'" >> "${BASH_ENV}"
