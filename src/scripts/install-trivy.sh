bb866b55c821dcb67fcc0fd0e25b8ea27c436b99
droguljic <1875821+droguljic@users.noreply.github.com>
2025-05-14 14:53:54 +0200

    feat: add `install_trivy` command
    The command to install Trivy open source security scanner.
    By default, the latest version is installed.
    Useful for cases when an image with preinstalled Trviy
    is not suitable, e.g. when the machine executor is needed.

---
+++ src/scripts/install-trivy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+BASE_URL="https://raw.githubusercontent.com/aquasecurity/trivy"
+INSTALL_SCRIPT_URL="${BASE_URL}/main/contrib/install.sh" 
+TRIVY_DEST_DIR="${TRIVY_DEST_DIR:-/usr/local/bin}"
+
+function install_trivy () {
+  local script_args=(-b "${TRIVY_DEST_DIR}")
+
+  if [[ -n "${PARAM_STR_VERSION}" ]]; then
+    script_args+=("${PARAM_STR_VERSION}")
+  fi
+
+  set -x
+  curl -sfL --retry 1 "${INSTALL_SCRIPT_URL}" | sudo sh -s -- "${script_args[@]}"
+  set +x
+
+  echo "Installed trivy ${PARAM_STR_VERSION:-latest} at ${TRIVY_DEST_DIR}"
+}
+
+if ! command -v trivy >/dev/null 2>&1; then
+  echo "Failed to detect trivy, installing..."
+
+  install_trivy
+fi
