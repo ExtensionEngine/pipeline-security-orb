e19d50eb6d5b2bc57fe7e83f98fa70e40d172d3c
droguljic <1875821+droguljic@users.noreply.github.com>
2025-05-21 17:58:28 +0200

    feat: add `generate_sbom` command (#46)
    The command generates SBOM from the Docker image.
    Under the hood uses Syft to accomplish this.
    Sources image from Docker daemon or tarball file from disk.

---
+++ src/scripts/generate-sbom.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+if [[ -z "${PARAM_STR_IMAGE}" ]]; then
+  echo "Specify image and retry."
+
+  exit 1
+fi
+
+IMAGE_SOURCE="docker"
+
+if [[ "${PARAM_STR_IMAGE}" == *.tar ]]; then
+  IMAGE_SOURCE="docker-archive"
+fi
+
+SBOM_ARGS=("${IMAGE_SOURCE}:${PARAM_STR_IMAGE}")
+SBOM_ARGS+=("-o" "${PARAM_ENUM_FORMAT}=${PARAM_STR_OUT_PATH}")
+
+if [[ -n "${PARAM_STR_EXCLUDE}" ]]; then
+  set -f # Disable glob expansion
+  for exclude_glob in ${PARAM_STR_EXCLUDE}; do
+    SBOM_ARGS+=("--exclude=$exclude_glob")
+  done
+  set +f
+fi
+
+set -x
+syft "${SBOM_ARGS[@]}"
+set +x
