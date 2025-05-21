bd89635993947a96eb632a24d0e30a124fe28124
droguljic <1875821+droguljic@users.noreply.github.com>
2025-05-21 15:23:22 +0200

    feat: add `install_syft` command (#44)
    The command to install Syft a CLI tool for generating SBOM. By default,
    the latest version is installed.
    Useful for cases when machine executor is needed in order to generate
    SBOM from image using Docker daemon as a source.

---
+++ src/scripts/install-syft.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+BASE_URL="https://raw.githubusercontent.com/anchore/syft"
+INSTALL_SCRIPT_URL="${BASE_URL}/main/install.sh" 
+SYFT_DEST_DIR="${SYFT_DEST_DIR:-/usr/local/bin}"
+
+function install_syft () {
+  local script_args=(-b "${SYFT_DEST_DIR}")
+
+  if [[ -n "${PARAM_STR_VERSION}" ]]; then
+    script_args+=("${PARAM_STR_VERSION}")
+  fi
+
+  set -x
+  curl -sfL --retry 1 "${INSTALL_SCRIPT_URL}" | sudo sh -s -- "${script_args[@]}"
+  set +x
+
+  echo "Installed syft ${PARAM_STR_VERSION:-latest} at ${SYFT_DEST_DIR}"
+}
+
+if ! command -v syft >/dev/null 2>&1; then
+  echo "Failed to detect syft, installing..."
+
+  install_syft
+fi
