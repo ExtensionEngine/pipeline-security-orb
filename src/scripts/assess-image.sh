5c5e7305f1090c6fed2d06f4e3ccacca6e087a67
droguljic <1875821+droguljic@users.noreply.github.com>
2025-05-22 16:54:52 +0200

    feat: add `assess_image` command (#47)
    The command scans Docker image for vulnerabilities and secrets.
    Under the hood uses Grype for the vulnerability scan and Trivy
    for the secret scan.
    Sources image from the Docker daemon of the tarball file from disk.

---
+++ src/scripts/assess-image.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+if [[ -z "${PARAM_STR_IMAGE}" ]]; then
+  echo "Specify image and retry."
+
+  exit 1
+fi
+
+
+function scan_secrets () {
+  local args=(image "--scanners=secret" "--image-config-scanners=secret")
+
+  if [[ "${PARAM_STR_IMAGE}" == *.tar ]]; then
+    args+=(--input)
+  fi
+
+  args+=("${PARAM_STR_IMAGE}")
+
+  set -x
+  trivy "${args[@]}"
+  set +x
+}
+
+function scan_vuln () {
+  local args=()
+
+  if [[ "${PARAM_STR_IMAGE}" == *.tar ]]; then
+    args+=("docker-archive:${PARAM_STR_IMAGE}")
+  else
+    args+=("docker:${PARAM_STR_IMAGE}")
+  fi
+  
+  if [[ -n "${PARAM_STR_IGNORE_FIX_STATUS}" ]]; then
+    args+=("--ignore-states=${PARAM_STR_IGNORE_FIX_STATUS}")
+  fi
+
+  if [[ -n "${PARAM_STR_EXCLUDE}" ]]; then
+    set -f # Disable glob expansion
+    for exclude_glob in ${PARAM_STR_EXCLUDE}; do
+      args+=("--exclude=$exclude_glob")
+    done
+    set +f
+  fi
+
+  args+=(--by-cve "--fail-on=${PARAM_ENUM_SEVERITY}")
+  args+=("--output=${PARAM_ENUM_REPORT_FORMAT}")
+  args+=("--file=${PARAM_STR_REPORT_PATH}")
+
+  set -x
+  grype "${args[@]}"
+  set +x
+}
+
+set -f # Disable glob expansion
+for scanner in ${PARAM_STR_SCANNERS}; do
+  if [[ $scanner == "vuln" ]]; then
+    scan_vuln
+  elif [[ $scanner == "secret" ]]; then
+    scan_secrets
+  else
+    echo "Unknow image scanner: $scanner"
+    exit 1
+  fi
+done
+set +f
